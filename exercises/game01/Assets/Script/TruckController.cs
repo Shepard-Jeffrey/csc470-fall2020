9a59fe00e1d772b990f3c6034e0b75c0958908a4
Shepard-Jeffrey <55208859+Shepard-Jeffrey@users.noreply.github.com>
2020-09-17 14:17:29 -0400

    test

---
+++ exercises/game01/Assets/Script/TruckController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TruckController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(gameObject.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+ 		float x = transform.position.x + transform.forward.x;   
+ 		float y = transform.position.y + transform.forward.y;      
+ 		float z = transform.position.z + transform.forward.z;      
+ 		transform.position = new Vector3(x, y, z);
+   
+    }
+}
